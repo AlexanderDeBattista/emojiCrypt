0a034b9bac6f2f6f37a21dbf4865accab8c9485f
Alexander De Battista Kvamme <alexander.kvamme@vipps.no>
2024-05-19 12:02:20 +0200

    Added models for encryption and decryption.

---
+++ emojiCrypt/App_Code/Models/Cipher.cs
@@ -0,0 +1,88 @@
+﻿using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace emojiCrypt.App_Code.Models
+{
+    public abstract class ICipher
+    {
+    }
+
+    public abstract class ISymmetricCipher<T, K> : ICipher
+    {
+        public abstract K Encrypt(T value, T key, T iv);
+        public abstract K Decrypt(T value, T key, T iv);
+    }
+
+
+    public class AesEcb : ISymmetricCipher<byte[], Task<byte[]>>
+    {
+        private Aes AesObject {  get; set; }
+        public AesEcb() {
+            AesObject = Aes.Create();
+        }
+
+        public async override Task<byte[]> Encrypt(byte[] Value, byte[] Key, byte[] Iv)
+        {
+            Task<byte[]> IvHash = this.HashValToBlocksize(Iv);
+            Task<byte[]> KeyHash = this.HashValToBlocksize(Key);
+
+            AesObject.IV = await IvHash;
+            AesObject.Key = await KeyHash;
+            return await Task.Run(() => AesObject.EncryptEcb(Value, PaddingMode.PKCS7));
+        }
+
+        public async override Task<byte[]> Decrypt(byte[] Value, byte[] Key, byte[] Iv)
+        {
+            Task<byte[]> IvHash = this.HashValToBlocksize(Iv);
+            Task<byte[]> KeyHash = this.HashValToBlocksize(Key);
+
+            AesObject.IV = await IvHash;
+            AesObject.Key = await KeyHash;
+            return await Task.Run(() => AesObject.DecryptEcb(Value, PaddingMode.PKCS7));
+        }
+
+        private async Task<byte[]> HashValToBlocksize(byte[] Iv)
+        {
+            return await Task.Run(() => SHA256.HashData(Iv).Take(AesObject.BlockSize / 8).ToArray());//Update to Keccak once supported
+        }
+
+    }
+    public class StringToByteCipher: ISymmetricCipher<string, Task<string>> 
+    {
+        private ISymmetricCipher<byte[], Task<byte[]>> SymmetricCipher { get; set; }
+        public StringToByteCipher(ISymmetricCipher<byte[], Task<byte[]>> Cipher)
+        {
+            SymmetricCipher = Cipher;
+        }
+        public async override Task<string> Encrypt(string Value, string Key, string Iv) {
+
+            byte[] encVal = await SymmetricCipher.Encrypt(
+                Encoding.UTF8.GetBytes(Value),
+                Encoding.UTF8.GetBytes(Key),
+                Encoding.UTF8.GetBytes(Iv)
+            );
+            return Convert.ToBase64String(encVal);
+        }
+
+        public async override Task<string> Decrypt(string Value, string Key, string Iv)
+        {
+
+            byte[] encVal = await SymmetricCipher.Decrypt(
+                Convert.FromBase64String(Value),
+                Encoding.UTF8.GetBytes(Key),
+                Encoding.UTF8.GetBytes(Iv)
+            );
+            return Encoding.UTF8.GetString(encVal);
+        }
+
+    }
+
+    public class AesEcbString : StringToByteCipher
+    {
+        public AesEcbString():base(new AesEcb())
+        {}
+    }
+}
+
+
---
+++ emojiCrypt/App_Code/Models/CipherData.cs
@@ -0,0 +1,67 @@
+﻿using System.Security.Cryptography;
+using System.Security.Cryptography.Xml;
+
+namespace emojiCrypt.App_Code.Models
+{
+    public abstract class ISymmetricCipherData<T>
+    {
+        private T? Plaintext {  get; set; }
+        private T? Ciphertext { get; set; }
+
+        private T? Key { get; set; }
+        private T? IV { get; set; }
+        public ISymmetricCipher<T, Task<T>> CipherMode;
+
+        public abstract Task<T> GetCiphertext();
+        public abstract Task<T> GetPlaintext();
+
+
+    }
+
+    public abstract class CipherDataString : ISymmetricCipherData<string>
+    {
+        CipherDataString(ISymmetricCipher<string, Task<string>> Cipher)
+        {
+            this.CipherMode = Cipher;
+        }
+        public class CipherDataStringAes : CipherDataString
+        {
+            private string? Plaintext { get; set; }
+            private string? Ciphertext { get; set; }
+
+            private string Key { get; set; }
+            private string Iv { get; set; }
+            CipherDataStringAes(string Key, string Iv) : base(new AesEcbString())
+            {
+                this.Key = Key;
+                this.Iv = Iv;
+            }
+
+            public async override Task<string> GetCiphertext()
+            {
+                if (this.Plaintext == null)
+                {
+                    throw new InvalidOperationException("Must define plaintext before you can decrypt");
+                }
+                if (this.Ciphertext == null)
+                {
+                    return await base.CipherMode.Encrypt(this.Plaintext, this.Key, this.Iv);
+                }
+                return this.Ciphertext;
+            }
+
+            public async override Task<string> GetPlaintext()
+            {
+                if (this.Ciphertext == null)
+                {
+                    throw new InvalidOperationException("Must define ciphertext before you can decrypt");
+                }
+                if (this.Plaintext == null)
+                {
+                    return await base.CipherMode.Decrypt(this.Ciphertext, this.Key, this.Iv);
+                }
+                return this.Plaintext;
+            }
+        }
+    }
+}
