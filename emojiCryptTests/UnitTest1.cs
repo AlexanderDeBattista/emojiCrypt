e75fc08c9cb0da6f2634e89a1d91c84cbb52e8ac
Alexander De Battista Kvamme <alexander.kvamme@vipps.no>
2024-05-20 12:12:29 +0200

    Tests for encrypting emojis

--- emojiCryptTests/UnitTest1.cs
+++ emojiCryptTests/UnitTest1.cs
@@ -1,4 +1,6 @@
 using emojiCrypt.App_Code.Models;
+using System.Drawing;
+using System.Text;
 
 namespace emojiCryptTests
 {
@@ -26,6 +28,87 @@ namespace emojiCryptTests
         public async Task EncDecSameResultLargePtxt()
         {
             await EncDecSameResult(1000);
+        }
+    }
+
+
+    public abstract class ICipherDataTest<T,K>
+    {
+            
+    }
+
+    [TestClass]
+    public class EmojiEncryptionTest
+    {
+   
+        public async Task CheckSameEmojisAfterEncAndDecB64(int sizeOfString)
+        {
+            string ptxt = new string('a', sizeOfString);
+            string key = "hello";
+            string iv = "yo";
+            EmojiArrayBase64String EmojiString = new EmojiArrayBase64String(Convert.ToBase64String(Encoding.UTF8.GetBytes(ptxt)));
+            CipherDataAesEmoji aesEmojiData = new CipherDataAesEmoji(key, iv);
+            CipherDataAesEmoji aesEmojiDataDecrypt = new CipherDataAesEmoji(key, iv);
+            aesEmojiData.SetPlaintext(EmojiString);
+
+            EmojiArrayBase64String ctxt = await aesEmojiData.GetCiphertext();
+
+            aesEmojiDataDecrypt.SetCiphertext(ctxt);
+
+            EmojiArrayBase64String NewPtxt = await aesEmojiDataDecrypt.GetPlaintext();
+
+            Console.WriteLine(ctxt.GetEmojiString());
+            Console.WriteLine(ctxt.GetEncodedString());
+
+            Assert.AreEqual(EmojiString.GetEmojiString(), NewPtxt.GetEmojiString());
+        }
+
+        [TestMethod]
+        public async Task SameAfterEncDecB64Small()
+        {
+            await CheckSameEmojisAfterEncAndDecB64(10);
+        }
+        [TestMethod]
+        public async Task SameAfterEncDecB64Large()
+        {
+            await CheckSameEmojisAfterEncAndDecB64(1000);
+        }
+
+        public async Task SameAfterEncDecString(string ptxt)
+        {
+            string key = "hello";
+            string iv = "yo";
+            EmojiArrayString EmojiString = new EmojiArrayString(ptxt);
+            CipherDataAesEmoji aesEmojiData = new CipherDataAesEmoji(key, iv);
+            CipherDataAesEmoji aesEmojiDataDecrypt = new CipherDataAesEmoji(key, iv);
+            aesEmojiData.SetPlaintext(EmojiString);
+
+            EmojiArrayString ctxt = new EmojiArrayString(await aesEmojiData.GetCiphertext());
+
+            aesEmojiDataDecrypt.SetCiphertext(ctxt);
+
+            EmojiArrayString NewPtxt = new EmojiArrayString(await aesEmojiDataDecrypt.GetPlaintext());
+
+            Console.WriteLine(EmojiString.GetEmojiString());
+            Console.WriteLine(NewPtxt.GetEmojiString());
+
+            Console.WriteLine(ctxt.GetEncodedString());
+
+            Console.WriteLine(EmojiString.RawString);
+            Console.WriteLine(NewPtxt.RawString);
+
+            Assert.AreEqual(EmojiString.GetEmojiString(), NewPtxt.GetEmojiString());
+        }
+
+        [TestMethod]
+        public async Task SameAfterEncDecStringSmall()
+        {
+            await SameAfterEncDecString("a");
+        }
+        [TestMethod]
+        public async Task SameAfterEncDecStringLarge()
+        {
+            await SameAfterEncDecString("I LOVE EMOJIIIIIIS (Am bigger than one block?)");
         }
     }
 }
